5c19f4af17f8696d057f95d94cfa0859b27faa8e
Chibueze Osuoji <qelvinz@gmail.com>
2024-07-01 14:58:01 +0100

    Added weather

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 import express from "express"
-import userip from "ip"
 import geoip from "geoip-lite"
+import { OpenWeatherAPI } from "openweather-api-node"
+
 
 const app = express()
 const PORT = 3000;
 app.set('trust proxy', true)
 
 
+
+
 app.get('/api',(req, res) => {
        
 
   var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-
   ip = ip.split(",")[0]
   var geo = geoip.lookup(ip);
-        res.json({
-            "client_ip": ip,
-            "location":  geo.city,
-            "greeting": `Hello ${req.query.visitor_name}`
 
-        });
-    });
+  let weather = new OpenWeatherAPI({
+    key: "6413e35f562912c5b0596ddcb372f384",
+    locationName: geo.city,
+    units: "Imperial"
+  })
+
+    weather.getCurrent().then(data => {
+    console.log(`Current temperature in Lagos is: ${data.weather.temp.cur}F`)
+    res.json({
+        "client_ip": ip,
+        "location": geo.city,
+        "greeting": `Hello ${req.query.visitor_name}, the weather is ${data.weather.temp.cur}F`
+    })
+})
+
+})
+//   res.json({
+//             "client_ip": ip,
+//             "location":  "her",
+//             "greeting": `Hello ${req.query.visitor_name}`
+
+//         });
+//     });
  
 app.listen(PORT,"0.0.0.0",
     function (err) {
