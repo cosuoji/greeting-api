0b649fa6fe4404bdedcd54599324f4277133ffad
Chibueze Osuoji <qelvinz@gmail.com>
2024-07-01 13:09:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express"
-import bodyParser from "body-parser"
-import ip from "ip"
+import userip from "ip"
 import geoip from "geoip-lite"
 
 const app = express()
@@ -10,8 +9,9 @@ const PORT = 3000;
 
 app.get('/api',(req, res) => {
        
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  let ip = req.socket.remoteAddress
   var geo = geoip.lookup(ip);
+  console.log(geo)
 
 
         res.json({
