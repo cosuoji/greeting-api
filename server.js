18616fe2d88cf18d1e8da61b67b9b35714c74059
cosuoji <56638390+cosuoji@users.noreply.github.com>
2024-07-01 16:38:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express"
-import geoip from "geoip-lite"
-import { OpenWeatherAPI } from "openweather-api-node"
+import geoip from "geoip-lite" //this package is responnsible for getting the location based on the IP address
+import { OpenWeatherAPI } from "openweather-api-node" //this, along with your open weather API allows you to detect the weather
 
 
 const app = express()
 const PORT = 3000;
-app.set('trust proxy', true)
+app.set('trust proxy', true) //helps you get the IP if it's behind a proxy
 
 
 
@@ -13,20 +13,21 @@ app.set('trust proxy', true)
 app.get('/api',(req, res) => {
        
 
-  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-  ip = ip.split(",")[0]
-  var geo = geoip.lookup(ip);
+  var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress //get's the IP addess
+  ip = ip.split(",")[0] // The ip address is in a string, but we need the first part of it 
+  var geo = geoip.lookup(ip); //lookup the location based on the IP
 
-  const nameOfUser = req.query.visitor_name || "Guest"
+  const nameOfUser = req.query.visitor_name || "Guest" 
 
+       //this is the open weather APU we use to check the weather of the city
   let weather = new OpenWeatherAPI({
     key: "6413e35f562912c5b0596ddcb372f384",
-    locationName: geo.city,
+    locationName: geo.city, //here we pass the city we get from checking the location earlier 
     units: "Imperial"
   })
 
-    weather.getCurrent().then(data => {
-    console.log(`Current temperature in Lagos is: ${data.weather.temp.cur}F`)
+       //Check the weather and send back the IP, location, name of user (if passsed to us) and the weather
+    weather.getCurrent().then(data => {   
     res.json({
         "client_ip": ip,
         "location": geo.city,
@@ -35,14 +36,7 @@ app.get('/api',(req, res) => {
 })
 
 })
-//   res.json({
-//             "client_ip": ip,
-//             "location":  "her",
-//             "greeting": `Hello ${req.query.visitor_name}`
 
-//         });
-//     });
- 
 app.listen(PORT,"0.0.0.0",
     function (err) {
         if (err) console.log(err);
